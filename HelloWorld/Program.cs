779d14814fbd041bc4bab9de2e0f9bfaa2362b8e
Stephen Afolabi <afolabistephen20@gmail.comcd .lsgit config --global user.email afolabistephen20@gmail.com>
2021-09-22 22:53:37 -0500

    User Platform

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,62 +1,60 @@
 ﻿using System;
 
-
-namespace HelloWorld
+ namespace HelloWorld
 {
    public class Program
-    {
+   {
 
-        static void Main(string[] args)
-        {        
-            string input = "";
-            bool exit = false;
+      public static void Main(string[] args)
+      {        
+                        string input = "";
+                        bool exit = false;
 
-         do 
-        {
-            Console.WriteLine("Welcome to BOP WAREHOUSE");
-            Console.WriteLine("No 1 leading Warehouse in the United States");
-            Console.WriteLine("Enter [1] for our Menus");
-            Console.WriteLine("Enter [2] to leave and read our reviews");
-            Console.WriteLine("Enter [3] to login or Create an account");
-            Console.WriteLine("Enter [4] to continue as a guest");
-            Console.WriteLine("Enter [0] to exit");
+                     do 
+         {
+                        Console.WriteLine("Welcome to BOP WAREHOUSE");
+                        Console.WriteLine("No 1 leading Warehouse in the United States");
+                        Console.WriteLine("Enter [1] for our Menus");
+                        Console.WriteLine("Enter [2] to leave and read our reviews");
+                        Console.WriteLine("Enter [3] to login or Create an account");
+                        Console.WriteLine("Enter [4] to continue as a guest");
+                        Console.WriteLine("Enter [0] to exit");
 
-            input = Console.ReadLine();
+                        input = Console.ReadLine();
 
-            switch(input)
-             {
+                        switch(input)
+                        {
 
-                 case "1":
-                            Console.WriteLine("You're about to read and view our reviews");
-           
-                   break;
+                           case "1":
+                                    Resto.Second();
+                                    
+                              break;
 
-                case "2":
-                   Console.WriteLine("You're about to read and view our reviews");
-                   break;
+                           case "2":
+                              Console.WriteLine("You're about to read and view our reviews");
+                              break;
 
-                case "3":
-                   Console.WriteLine("You're about to Login or Create an Account");
-                   break;
-                case "4":
-                   Console.WriteLine("You're about to Continue as a guest");
-                   break;
-                case "0":
-                   Console.WriteLine("We're sorry to see you leave");
-                   exit = true;
-                   break;
-                default:
-                   Console.WriteLine("Invalid Number!");
-                   break;
-                }
-
-             }while(!exit);
+                           case "3":
+                              Console.WriteLine("You're about to Login or Create an Account");
+                              break;
+                           case "4":
+                              Console.WriteLine("You're about to Continue as a guest");
+                              break;
+                           case "0":
+                              Console.WriteLine("We're sorry to see you leave");
+                              exit = true;
+                              break;
+                           default:
+                              Console.WriteLine("Invalid Number!");
+                              break;
+                           }
+                        
+         }while(!exit);
+      }
             
-      
-        }
-    
-     
-          
-    }
+   }
 
+                  
 }
+                  
+                  
